706fe1413f0315ae4161ce0631e15062089d20f2
nepos466 <nepos466@gmail.com>
2015-05-18 19:44:59 -0500

    Update LenguajesIvannl.java

--- src/lenguajes/ivannl/LenguajesIvannl.java
+++ src/lenguajes/ivannl/LenguajesIvannl.java
@@ -15,7 +15,7 @@ public class LenguajesIvannl {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Mi primer proyecto");
+        System.out.println("Mi primer proyecto CON ESTA COSA TAN BONITA :3");
     }
     
 }
