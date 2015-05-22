7a3fdb705592868ee30f6679319285a3f0bfc5f8
T-107 <T-107@PC261>
2015-05-21 19:43:17 -0500

    2da Clase

---
+++ src/abstraccion/Imc.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author T-107
+ */
+public class Imc {
+    float peso;
+    float altura;
+    
+    public Imc(float p, float a){
+    peso=p;
+    altura=a;
+    }
+
+    public float calcular (){
+    float imc=peso/(altura*altura);
+    return imc;
+       
+    }
+}
