f42764f893c5ec6f6280f42e6348f2e1c9bc7e3e
Soporte <Soporte@cpe-172-90-22-100.socal.res.rr.com>
2015-05-28 11:51:00 -0500

    Pruebas y reglas en  java

---
+++ src/constructores/Probar2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author Soporte
+ */
+public class Probar2{
+
+public static void main (String argas[]) {
+Test2 objeto1= new Test2(5.8f,12);
+Test2 objeto2= new Test2(12,5.8f);
+                                         }
+}
---
+++ src/constructores/Test2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+
+/**
+ *
+ * @author Soporte
+ */
+public class Test2 {
+int x;
+float y;
+
+public Test2 (int x, float y){  }
+public Test2 (float x, int y) {  }
+//public Test2 (int y, float x ){  }
+}
+// regla no importa que haya cambio de pocision en las variables exepto cuando, las variables se duplican abra un error 
+// ya que Java dice que ya hay un asignamiento de variables previo.
+
+
