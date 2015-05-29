2b020d7ab22f916b99d557090b06831734713331
T-107 <T-107@PC260>
2015-05-28 19:41:08 -0500

    Clase 28/mayo/2015

--- src/abstraccion/Imc.java
+++ src/abstraccion/Imc.java
@@ -10,9 +10,23 @@ package abstraccion;
  * @author T-107
  */
 public class Imc {
-    float peso;
-    float altura;
+    private float peso;
+    private float altura;
+    // aqui declaramos un atributo para comprobar al boolean, ya que los constructores inicializan los valores por defecto
+    // de los atributos
+    private boolean flaco;
     
+    ///
+
+
+    public void setFlaco(boolean flaco) {
+        this.flaco = flaco;
+    }
+    
+    public boolean isFlaco(){
+    return flaco;
+    }
+    //// 
     public Imc(float p, float a){
     peso=p;
     altura=a;
@@ -22,5 +36,24 @@ public class Imc {
     float imc=peso/(altura*altura);
     return imc;
        
+    }
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    public float getAltura() {
+        return peso;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+   
+    public Imc(){
+    
     }
 }
--- src/abstraccion/InterfazImc.java
+++ src/abstraccion/InterfazImc.java
@@ -5,19 +5,40 @@
  */
 package abstraccion;
 
-/**
- *
- * @author T-107
- */
 public class InterfazImc {
     
    public static void main(String oso[]){
    
        //  objeto    constructor
        Imc imc=new Imc(80,1.67f);
+       imc.setPeso(90);
    
        System.out.println(imc.calcular());
-           
+       System.out.println(imc.getPeso());
     
+       
+  // Primitivos integrales
+       byte b=2;
+       short s=2;
+       int i=2;
+       long l=2; 
+       
+      b=(byte)s;
+      s=b;
+      i=(int)l;
+      i=s;
+      s=(short)i;
+      // el double es el valor por defecto
+      float f=2.0f;
+      double d=2.0;
+      
+     // el valor de false es por defecto
+     
+     boolean valor=false;
+     // el constructor por defecto el false porque inicializa el valor por defecto de todos los atributos
+     Imc otro=new Imc();
+     otro.setFlaco(true);
+       System.out.println(otro.isFlaco());
+     
 }
 }
